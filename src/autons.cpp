0f20c6fb37ecaeb3727203643be1e6c81955593c
MerilnE <soren.richards@hotmail.com>
2025-01-31 18:54:14 -0600

    Red Route Skills movement
    Finished the movement for the 15" robot skills run

--- src/autons.cpp
+++ src/autons.cpp
@@ -143,70 +143,89 @@ void red_route_skills(){
       //   claw.moveTo(INTAKE);
       // }).detach();
 
-
-
-      chassis.turn_to_angle(180);
-
-
-
-    // chassis.Gyro.setHeading(300, deg);
-    // chassis.drive_distance(-21);
-    // //clamp
-    // chassis.turn_to_angle(245);
-
     
-    // chassis.drive_distance(10);
-    // //spin intake
-    // chassis.turn_to_angle(288);
-    // chassis.drive_distance(22);
-    // //spin intake
-    // chassis.turn_to_angle(44);
-    // chassis.drive_distance(44);
-    // //spin intake
-    // chassis.turn_to_angle(47);
-    // chassis.drive_distance(28);
-    // //spin intake
-    // chassis.turn_to_angle(20);
-    // chassis.drive_distance(-11);
-    // chassis.turn_to_angle(47);
-    // chassis.drive_distance(13);
-    // //spin intake
-    // chassis.drive_distance(-74);
-    // //clamp release
-    // chassis.turn_to_angle(67);
-    // chassis.drive_distance(48);
-    // chassis.turn_to_angle(248);
-    // chassis.drive_distance(-31);
-    // //clamp
 
 
-    // //STEP TWO
 
-    // //set claw to intake
-    // chassis.turn_to_angle(215);
-    // chassis.drive_distance(16);
-    // //claw grab
-    // //set claw to wall
-    // chassis.turn_to_angle(220);
-    // chassis.drive_distance(8);
-    // chassis.turn_to_angle(180);
+      
+
+
+    chassis.set_heading(305);
+    chassis.drive_distance(-21);
+    //clamp
+    chassis.turn_to_angle(245);    
+    chassis.drive_distance(11);
+    //spin intake
+    chassis.turn_to_angle(228);
+    chassis.drive_distance(21);
+    //spin intake
+
+
+    chassis.turn_to_angle(44);
+    chassis.drive_distance(44);
+    //spin intake
+    chassis.turn_to_angle(47);
+    chassis.drive_distance(28);
+    //spin intake
+    chassis.turn_to_angle(20);
+    chassis.drive_distance(-11);
+    chassis.turn_to_angle(47);
+    chassis.drive_distance(13);
+    //spin intake
+    chassis.drive_distance(-74);
+    //clamp release
+    chassis.turn_to_angle(63);
+    chassis.drive_distance(48);
+    chassis.turn_to_angle(266);
+    chassis.drive_distance(-27);
+    // chassis.turn_to_angle(232);
     // chassis.drive_distance(6);
-    // //spin intake
-    // chassis.drive_distance(7);
-    // //score wall
-    // chassis.turn_to_angle(60);
-    // chassis.drive_distance(21);
-    // //intake
-    // chassis.turn_to_angle(48);
-    // chassis.drive_distance(32);
-    // //intake
-    // chassis.turn_to_angle(164);
-    // chassis.drive_distance(11);
-    // //intake
-    // chassis.turn_to_angle(143);
-    // chassis.drive_distance(25);
+    //clamp
 
-    // //STEP THREE
+
+    //STEP TWO
+
+    //set claw to intake
+    chassis.turn_to_angle(223);
+    chassis.drive_distance(12);
+    //claw grab
+    //set claw to wall
+    chassis.turn_to_angle(223);
+    chassis.drive_distance(7);
+    chassis.turn_to_angle(180);
+    chassis.drive_distance(4);
+    //spin intake
+    chassis.drive_distance(6);
+    //score wall
+    chassis.turn_to_angle(60);
+    chassis.drive_distance(21);
+    //intake
+    chassis.turn_to_angle(48);
+    chassis.drive_distance(32);
+    //intake
+    chassis.turn_to_angle(164);
+    chassis.drive_distance(11);
+    //intake
+    chassis.turn_to_angle(143);
+    chassis.drive_distance(25);
+
+    //STEP THREE
+
+    chassis.turn_to_angle(358);
+    chassis.drive_distance(52);
+    //claw grab
+    //claw alliance
+    chassis.turn_to_angle(0);
+    chassis.drive_distance(8);
+    chassis.turn_to_angle(90);
+    //score
+    chassis.drive_distance(-12);
+    //clamp
+    chassis.turn_to_angle(0);
+    chassis.drive_distance(16);
+    //spin intake
+
+
 
 
 
